63512da083acd50bf4df1720ed5700dbd692be5f
LaserLinx <135711444+LaserLinx@users.noreply.github.com>
2025-03-23 14:51:52 +0100

    Add files via upload

---
+++ ItemCopy.py
@@ -0,0 +1,18 @@
+import os
+#os.system("pip install pyperclip")
+import pyperclip
+import inventory
+def copy_to_clipboard(text):
+	pyperclip.copy(text)
+		
+def copy_selected_item():
+	item = inventory.get_selected_item()
+	print(f"[selected item]: {item}")
+	copy_to_clipboard(str(item))
+
+
+RUN_DIALOG_NAME = "Copy Selected Item"
+def runDialog():
+	copy_selected_item()
+def run():
+	print("[Item Copy]: ItemCopy Now running!")
---
+++ m_debug.py
@@ -0,0 +1,29 @@
+from helpers import DataAPI
+
+def on_mouse_move(event):
+	global m_debug
+	if m_debug:
+		print(f"Mouse position: x={event.x}, y={event.y}")
+
+def update():
+	try:
+		frame.bind("<Motion>", on_mouse_move)
+	except:
+		pass
+
+m_debug = False
+
+RUN_DIALOG_NAME = "Toogle m Debug"
+def runDialog():
+	global m_debug
+	if m_debug:
+		m_debug = False
+		print("[info]: m debug off")
+	else:
+		print("[info]: m debug on")
+		m_debug = True
+def run():
+	global frame
+	frame = DataAPI.root
+	print("[m debug]: m Debug Now running!")
+
