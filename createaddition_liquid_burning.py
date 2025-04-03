750460491a6bd0374c0aa60f5055446cb7334714
MaxTechnik <144486710+MaxTechnik3796@users.noreply.github.com>
2025-04-03 18:53:57 +0200

    MT-Update #0001

---
+++ createaddition_liquid_burning.py
@@ -0,0 +1,37 @@
+#+================================+
+#|                                |
+#|      Made by: MaxTechnik       |
+#|   (Leserová práce: 0.0000001%) |
+#+================================+
+import craftixtools as ct
+import json
+def run():
+	ct.ok(f"running {__name__}")
+SCRUCTURE = {
+	"createaddition_liquid_burning": {
+		"fluid_input": "",
+		"fluid_amount":"1000",
+		"time":"200",
+		"heat":"0"
+	}
+}
+
+ct.CreateScructure(SCRUCTURE)
+def PlayGround():
+	offset:int=120
+	ct.Input("fluid_amount",483+offset,190,width=100)
+	ct.ItemInput("fluid_input",348+offset,159)
+	ct.caption("Fluid:",292+offset,191)
+	ct.caption("Amount:",417+offset,190)
+	ct.Input("time",483+offset,226,width=100)
+	ct.caption("Time:",431+offset,226)
+	ct.CheckBox("heat","Super-Heated",290+offset,228,width=100,height=24)
+#generator
+def Generator(js):
+    rec={}
+    rec["type"]="createaddition:liquid_burning"
+    rec["burnTime"]=int(js.get("time"))
+    rec["superheated"]=bool(js.get("heat"))
+    rec["input"]={"fluid":js.get("fluid_input"),"amount":int(js.get("fluid_amount"))}
+    
+    return json.dumps(rec)
